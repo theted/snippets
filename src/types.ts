c41a1be26d6937fc0f248f7232c343f20e1ea9c1
Fredrik Sundberg <fredrik.sundberg@afry.com>
2021-12-20 23:02:53 +0100

    Use Zod for snippet schema generation/validation

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,18 @@
-export type Snippet = {
-  id: string;
-  title: string;
-  content: string;
-  description?: string;
-  language?: string;
-  tags?: string[];
-}
+import { z } from 'zod';
+
+const Tag = z.string();
+
+export const SnippetSchema = z.object({
+  id: z.string(),
+  userId: z.number().optional(),
+  title: z.string(),
+  content: z.string(),
+  description: z.string().optional(),
+  language: z.string().optional(),
+  tags: z.array(Tag).optional(),
+  private: z.boolean().optional(),
+});
+
+export type Snippet = z.infer<typeof SnippetSchema>;
+
+export const CreateSnippetSchema = SnippetSchema.omit({ id: true });
