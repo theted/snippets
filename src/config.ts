7d4bede4f41a98432275d5c7c289588536cae150
Fredrik Sundberg <ted.sundberg@gmail.com>
2026-02-02 10:39:40 +0100

    Migrate to Vite

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT ?? 'development';
+export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT ?? 'development';
 
-export const API_BASE = process.env.REACT_APP_API_BASE ?? 'http://localhost:3200';
+export const API_BASE = import.meta.env.VITE_API_BASE ?? 'http://localhost:3200';
 
 export const AVAILABLE_LANGUAGES = [
   'javascript',
