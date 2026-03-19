f3f16f72c22241bc6a96d40c4f3bc6d3cc62d938
Fredrik Sundberg <ted.sundberg@gmail.com>
2026-03-20 00:44:17 +0100

    Add improved instant spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { useIsFetching } from '@tanstack/react-query';
 
 export const SpinFigure = () => (
-  <svg className="spinner" viewBox="0 0 50 50">
-    <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
-  </svg>
+  <div className="spin-ring" />
 );
 
 const Spinner: React.FC = () => {
